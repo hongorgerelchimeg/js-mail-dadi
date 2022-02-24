600062c8663b814bcee2c369c6f2ae643403d8c9
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-02-24 16:44:43 +0100

    Creato array di emails
    creato ciclo per cercare email esiste o meno

--- mail/scripts/script.js
+++ mail/scripts/script.js
@@ -1,37 +1,67 @@
-// Creamo random mail
-// Questa funziona crea random domain mail
-// domain valore = gmail, boolean, yahoo
-function randomMail(limit) {
+// Array of emails
+const emailList = ['booleaner1@gmail.com', 'booleaner2@gmail.com', 'booleaner3@gmail.com', 'booleaner4@gmail.com', 'booleaner5@gmail.com', 'booleaner6@gmail.com'];
 
-    let genEmail = '';
-    let domainName = '@gmail.com';
-    let domainName2 = '@boolean.com';
-    let domainName3 = '@yahoo.com';
-    let result ;
-  
-    for (let i = 1; i <= limit; i++) {
-      let address = Math.random().toString(36).substr(2, 8);
-      let counter = Math.round(getRandomInt(0, 2));
-      if (counter === 0) {
-          genEmail = address + domainName;
-      } else if (counter === 1) {
-          genEmail = address + domainName2;
-      } else if (counter === 2) {
-          genEmail = address + domainName3;
-      }
-     
-      console.log(genEmail);
-      
+console.log(emailList);
+
+// Client search email
+
+const searchMail = prompt('scrivi email')
+
+// Cycle for searching email from list
+
+let found = false;
+
+for (let i = 0; i < emailList.length; i++) {
+    if (emailList[i] == searchMail) {
+        found = true;
     }
-  }
+}
+
+console.log('found', found)
+
+// output answer 
+
+if (found == false) {
+    console.log('non esiste');
+}  else {
+    console.log('esiste');
+}
+
+
+
+
+// // Creamo random mail
+// // Questa funziona crea random domain mail
+// // domain valore = gmail, boolean, yahoo
+// function randomMail(limit) {
+
+//     let genEmail = '';
+//     let domainName = '@gmail.com';
+//     let domainName2 = '@boolean.com';
+//     let domainName3 = '@yahoo.com';
+//     let result = document.querySelector('#test');
   
-// Qui difinesce quanti mail vogliamo
-
-  randomMail(100)
+//     for (let i = 1; i <= limit; i++) {
+//       let address = Math.random().toString(36).substr(2, 8);
+//       let counter = Math.round(getRandomInt(0, 2));
+//       if (counter === 0) {
+//           genEmail = address + domainName;
+//       } else if (counter === 1) {
+//           genEmail = address + domainName2;
+//       } else if (counter === 2) {
+//           genEmail = address + domainName3;
+//       }
+     
+      
+//     }
+//   }
   
-  function getRandomInt(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+// // Qui difinesce quanti mail vogliamo
+
+//   randomMail(100)
+  
+//   function getRandomInt(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
 
 
- ;
